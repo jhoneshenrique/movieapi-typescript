42964b245106b9d81df203e8b8d11b48c12aa31f
Jhones Henrique <jhoneshenriquedasilva@gmail.com>
2022-03-15 10:54:43 -0300

    Creating files .env, .gitignore, and changing atlas database credentials

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,9 @@
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
 export default {
     port: 3000,
-    dbUri: "mongodb+srv://jhones:TC1o5vqS8qrthIPZ@cluster0.gfwc0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" 
+    
+    
+    dbUri: "mongodb+srv://"+dbUser+":"+dbPassword+"@cluster0.gfwc0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" 
 };
