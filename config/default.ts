ac52b74c2904f874e821cb43ccbf8170f4ba3da5
Jhones Henrique <jhoneshenriquedasilva@gmail.com>
2022-03-15 10:32:24 -0300

    Connecting to mongoDB server on Atlas server

--- config/default.ts
+++ config/default.ts
@@ -1,3 +1,4 @@
 export default {
-    port: 3000
-}
+    port: 3000,
+    dbUri: "mongodb+srv://jhones:TC1o5vqS8qrthIPZ@cluster0.gfwc0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" 
+};
--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,9 @@ const app = express()
 //JSON middleware
 app.use(express.json())
 
+// DB
+import db from "../config/db"
+
 //Routes
 import router from "./router"
 
@@ -18,5 +21,8 @@ app.use("/api/", router)
 const port = config.get<number>("port");
 
 app.listen(3000, async() =>{
+
+    await db();
+
     console.log("Aplicação rodando na porta "+port)
 });
