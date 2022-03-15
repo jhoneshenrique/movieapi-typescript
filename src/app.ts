31f55a95f9b89b92cc317d255e0dc1d56ae23675
Jhones Henrique <jhoneshenriquedasilva@gmail.com>
2022-03-15 09:38:17 -0300

    Setting up express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+import express from "express"
+import config from "config"
+
+const app = express()
+
+//JSON middleware
+app.use(express.json())
+
+// App port
+const port = config.get<number>("port");
+
+app.listen(3000, async() =>{
+    console.log("Aplicação rodando na porta 3000 "+port)
+});
